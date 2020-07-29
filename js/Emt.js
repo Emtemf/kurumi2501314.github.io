637d963afcf89b8d3b1c98938f8997051f6fea50
kurumi2501314 <1732483487@qq.com>
2020-07-29 20:06:51 +0800

    Site updated: 2020-07-29 20:06:47

--- js/Emt.js
+++ js/Emt.js
@@ -8,9 +8,9 @@ $(document).ready(function(e){
     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> EMT');
 })
 
-// // 移除页脚样式
-// var app = document.getElementById('footer');
-// app.removeAttribute('id')
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
 
 /*添加图片top*/
 var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
