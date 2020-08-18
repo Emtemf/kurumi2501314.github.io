69a5b2def84a4fede241b59dcca8118d55155af3
kurumi2501314 <1732483487@qq.com>
2020-08-18 11:52:19 +0800

    Site updated: 2020-08-18 11:52:11

--- js/Emt.js
+++ js/Emt.js
@@ -12,14 +12,14 @@ $(document).ready(function(e){
     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> EMT');
 })
 
-/* 手机客户端导航栏默认隐藏 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
+// /* 手机客户端导航栏默认隐藏 */
+// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+// var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+// var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+// for (var i = 0; i < menus_item_child.length; i++) {
+//     menus_item_child[i].style.display = "none";
+//     menus_expand[i].className += " closed";
+// }
 
 // 可爱的Title
 var OriginTitle = document.title;
